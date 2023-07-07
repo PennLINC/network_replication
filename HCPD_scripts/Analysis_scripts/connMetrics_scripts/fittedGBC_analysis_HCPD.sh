68a5f06b72aed791692379738e1aedf5d24edddb
audreycluo <audrey.luo@pennmedicine.upenn.edu>
2023-07-07 14:08:42 -0400

    Updated hcpd

--- HCPD_scripts/Analysis_scripts/connMetrics_scripts/fittedGBC_analysis_HCPD.sh
+++ HCPD_scripts/Analysis_scripts/connMetrics_scripts/fittedGBC_analysis_HCPD.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+
+
+# cd /cbica/projects/network_replication/Rscripts/HCPD_scripts/Analysis_scripts/connMetrics_scripts
+# qsub -l h_vmem=25G,s_vmem=25G fittedGBC_analysis_HCPD.sh
+
 Rscript --save /cbica/projects/network_replication/Rscripts/functions/main_analyses/fittedGBC_analysis.R "HCPD" "GBC"
