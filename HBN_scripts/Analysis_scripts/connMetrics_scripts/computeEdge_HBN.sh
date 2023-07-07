745eaed862ac5392f62a6278262e0dced3f80ee3
audreycluo <audrey.luo@pennmedicine.upenn.edu>
2023-07-07 11:06:28 -0400

    update HBN connMatrices and connMetrics files

--- HBN_scripts/Analysis_scripts/connMetrics_scripts/computeEdge_HBN.sh
+++ HBN_scripts/Analysis_scripts/connMetrics_scripts/computeEdge_HBN.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# cd /cbica/projects/network_replication/Rscripts/HBN_scripts/Analysis_scripts/connMetrics_scripts/
+# qsub -l h_vmem=25G,s_vmem=25G computeEdge_HBN.sh 
 Rscript --save /cbica/projects/network_replication/Rscripts/functions/main_analyses/computeEdge.R "HBN"
--- HBN_scripts/Analysis_scripts/connMetrics_scripts/covbat_edges_HBN.sh
+++ HBN_scripts/Analysis_scripts/connMetrics_scripts/covbat_edges_HBN.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# cd /cbica/projects/network_replication/software/docker
+# singularity pull docker://audreycluo/r-packages-for-cubic:r_forNetworkReplication0.0.3
+
+# cd /cbica/projects/network_replication/Rscripts/HBN_scripts/Analysis_scripts/connMetrics_scripts/
+# qsub -l h_vmem=64G,s_vmem=64G covbat_edges_HBN.sh
+
 singularity run --cleanenv \
-    /cbica/projects/network_replication/software/docker/r-packages-for-cubic_r4.1.2forNetworkReplication.sif \
+    /cbica/projects/network_replication/software/docker/r-packages-for-cubic_r_forNetworkReplication0.0.3.sif \
     Rscript --save /cbica/projects/network_replication/Rscripts/functions/main_analyses/covbat_edges.R "HBN"
 
  
